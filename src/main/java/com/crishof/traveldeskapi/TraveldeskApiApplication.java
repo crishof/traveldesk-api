6299af4ae1431f9f045d4dec91c58dce72070b93
crishof <cristianhof@gmail.com>
2026-03-21 05:34:24 -0300

    first commit

---
+++ src/main/java/com/crishof/traveldeskapi/TraveldeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.crishof.traveldeskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveldeskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraveldeskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crishof/traveldeskapi/TraveldeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crishof.traveldeskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveldeskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
