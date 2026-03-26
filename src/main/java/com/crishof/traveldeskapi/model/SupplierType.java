003f4a4d379a115b38e7037ce584fcda96e0a977
Cristian Hoffmann <cristianhof@gmail.com>
2026-03-26 19:25:45 -0300

    add supplier types

--- src/main/java/com/crishof/traveldeskapi/model/SupplierType.java
+++ src/main/java/com/crishof/traveldeskapi/model/SupplierType.java
@@ -2,10 +2,15 @@ package com.crishof.traveldeskapi.model;
 
 public enum SupplierType {
     AIRLINE,
-    HOTEL,
-    TRANSPORT,
-    TOUR_OPERATOR,
-    OPERATOR,
+    AIR_CONSOLIDATOR,
+    BED_BANK,
+    TOUR_OPERATOR,          // mayorista / paquetes
+    TRANSFER,
+    CRUISE,
+    FERRY,
+    TRAIN,
+    TICKET_PROVIDER,        // entradas a espectáculos / eventos
+    LOCAL_TOUR_OPERATOR,    // tours y excursiones locales
     INSURANCE,
     OTHER
 }
